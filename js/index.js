ee134b6ff24b2795258fd65b9f850498824c98d1
Lfelipe-Freitas <lfelipe.dfreitas@gmail.com>
2022-10-04 17:12:37 -0300

    Implementação da página de sorteios

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
-import usuarios from "./moduloDados.js";
-console.log(usuarios)
+/**
+ * Trouxe o array do arquivo moduloDados.js
+ * para o index.js
+ */
+ import usuarios from "./moduloDados.js";
+
+const btnEntrar = document.getElementById('btnEntrar')
+const alertaLogin = new bootstrap.Modal('#alertaLogin')
+
+btnEntrar.addEventListener('click', () => {
+    const inputlogin = document.getElementById('login').value
+    const inputSenha = document.getElementById('senha').value
+
+    let usuarioValido
+
+    for(let usuario of usuarios){
+        if(usuario.nomeUsuario == inputlogin){
+            usuarioValido = usuario
+            break
+        }else{
+            alertaLogin.show()
+        }
+    }
+
+    if(inputSenha == usuarioValido.senha){
+        //encaminhado o usuário para outra página
+        window.location.pathname = '/app,html'
+
+    }else{
+        alertaLogin.show()
+    }
+})
