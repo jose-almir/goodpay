91c55c7868e45f7be73cd7e3ab03d85744bad592
marianagsoares <marianasoares.ti@gmail.com>
2022-10-06 10:30:28 -0300

    Implementar SEO na página index.html

---
+++ js/meta-tag.js
@@ -0,0 +1,18 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="authors" content="SoulCode, JFS5">
+        <meta name="description" content="Fintech brasileira segura e sustentável que dispõe das menores taxas de juros do mercado. Na GoodPay seu dinheiro rende acima da inflação">
+        <meta name=”robots” content="index, nofollow">
+        <meta property="og:title" content="[money fo good]"/>
+        <meta property="og:url" content="https://jose-almir.github.io/goodpay/"/>
+        <meta property="og:image" content="https://jose-almir.github.io/goodpay/img/logo/GoodPayBanner.png"/>
+        <meta property="og:description" content="Fintech brasileira segura e sustentável que dispõe das menores taxas de juros do mercado. Na GoodPay seu dinheiro rende acima da inflação"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt_BR" />
+`
+
+document.head.appendChild(template.content)
